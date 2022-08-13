7ac5f146b964e645303a79f676f07aa83e751892
Gekon <grzegorz.szustkiewicz@gmail.com>
2022-08-13 10:01:53 +0200

    Add CustomScanner.java

---
+++ CustomScanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CustomScanner {
+
+/*    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imie swe prosze");
+        String name = scanner.nextLine();
+        System.out.println("Czesc " + name + "!");
+    }*/
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int secondNumber = scanner.nextInt();
+        int thirdNumber = firstNumber + secondNumber;
+        System.out.println("And the result of adding these numbers is: " + thirdNumber);
+
+    }
+}
